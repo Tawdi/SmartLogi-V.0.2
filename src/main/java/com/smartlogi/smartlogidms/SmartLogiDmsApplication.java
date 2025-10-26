5067e9d3176f01b91e6c2c16cd751b150cbdb333
Tawdi <ahmedtawdi00@gmail.com>
2025-10-26 22:49:33 +0100

    INIT

---
+++ src/main/java/com/smartlogi/smartlogidms/SmartLogiDmsApplication.java
@@ -0,0 +1,13 @@
+package com.smartlogi.smartlogidms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogiDmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLogiDmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartlogi/smartlogidms/SmartLogiDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlogi.smartlogidms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLogiDmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
