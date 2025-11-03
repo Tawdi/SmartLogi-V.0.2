d72a31955ba32a59fc32aea1504dbce1102dd10d
Tawdi <ahmedtawdi00@gmail.com>
2025-11-04 00:26:33 +0100

    FEAT : add search to ClientContoller

--- src/main/java/com/smartlogi/smartlogidms/masterdata/zone/api/ZoneController.java
+++ src/main/java/com/smartlogi/smartlogidms/masterdata/zone/api/ZoneController.java
@@ -1,11 +1,14 @@
 package com.smartlogi.smartlogidms.masterdata.zone.api;
 
 import com.smartlogi.smartlogidms.common.api.controller.StringBaseController;
+import com.smartlogi.smartlogidms.common.api.dto.ApiResponseDTO;
+import com.smartlogi.smartlogidms.common.api.dto.ValidationGroups;
 import com.smartlogi.smartlogidms.masterdata.zone.domain.Zone;
 import com.smartlogi.smartlogidms.masterdata.zone.service.ZoneService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/zones")
@@ -19,5 +22,11 @@ public class ZoneController extends StringBaseController<Zone, ZoneRequestDTO, Z
         super(zoneService, zoneMapper);
         this.zoneService = zoneService;
         this.zoneMapper = zoneMapper;
+    }
+
+    @PutMapping("/{id}/a")
+    public ResponseEntity<ApiResponseDTO<ZoneResponseDTO>> update(@PathVariable String id, @Validated(ValidationGroups.Update.class)  @RequestBody ZoneRequestDTO requestDTO) {
+        ZoneResponseDTO responseDTO = service.update(id, requestDTO);
+        return ResponseEntity.ok(ApiResponseDTO.success("Resource updated successfully", responseDTO));
     }
 }
